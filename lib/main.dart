5afdf7e724ea6ba0abd9cae9424b2a410f05874d
bruno <bruno.pfsilva@hotmail.com>
2019-07-28 22:07:07 +0100

    incrementado leitura,escrita de json como banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -12,19 +18,41 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _listTodo = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("OK"),
-
       ),
-
       body: new Center(
         child: new Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
         ),
       ),
     );
+  }
+
+  //retorna um dado futuro
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_listTodo);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  //ler arquivos
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
   }
 }
