6ee316821155dcc88af29655d1ede8817b54fd21
bruno <bruno.pfsilva@hotmail.com>
2019-07-29 20:46:02 +0100

    implementando listiview

--- lib/layout.dart
+++ lib/layout.dart
@@ -7,9 +7,15 @@ class Layout {
 
 
     return Scaffold(
+
       appBar: AppBar(
-        backgroundColor: Color.fromRGBO(255, 250, 250,1),
+        //backgroundColor: Color.fromRGBO(255, 250, 250,1),
+        backgroundColor: Colors.blueAccent,
+        title: Text("Lista de tarefas"),
+        centerTitle: true,
+
       ),
+      body: content,
     );
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+//layout
+import 'layout.dart';
+
 import 'package:flutter/material.dart';
 
 import 'package:path_provider/path_provider.dart';
@@ -22,16 +25,47 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("OK"),
-      ),
-      body: new Center(
-        child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-        ),
+    final content = Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          //container para da espaçamento no app
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: <Widget>[
+                //Expanded oculpa o maximo de espaço possivel na largura
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Nova tarefa",
+                      labelStyle: TextStyle(color: Colors.blueAccent),
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  child: Text("ADD"),
+                  textColor: Colors.white,
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                padding: EdgeInsets.only(top: 10.0),
+                itemCount: _listTodo.length,
+                itemBuilder: (context, index) {
+
+                }),
+          )
+        ],
       ),
     );
+
+    //layout base separado
+    return Layout.getContent(context, content);
   }
 
   //retorna um dado futuro
