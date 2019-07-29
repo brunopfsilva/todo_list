e819ec9abde48b5f1c18d8906cbeb1a31befa53f
bruno <bruno.pfsilva@hotmail.com>
2019-07-29 16:09:10 +0100

    novo arquivo layout

---
+++ lib/layout.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Layout {
+
+  static Scaffold getContent(BuildContext context,content){
+
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(255, 250, 250,1),
+      ),
+    );
+  }
+
+}
